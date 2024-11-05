c1beff3c2543702b2f05cbcce1052a19db024f69
MarcP70 <marcpourias@yahoo.fr>
2024-11-05 17:15:40 +0100

    Update 2-password.dart to check the length of the password

--- 0x02-dart-classes/2-password.dart
+++ 0x02-dart-classes/2-password.dart
@@ -2,6 +2,8 @@ class Password {
   String password = "";
 
   bool isValid() {
+    // Vérifie si le mot de passe contient entre 8 et 16 caractères
+    bool isCorrectLength = password.length >= 8 && password.length <= 16;
     // Vérifie si le mot de passe contient au moins une majuscule
     bool hasUppercase = password.contains(RegExp(r'[A-Z]'));
     // Vérifie si le mot de passe contient au moins une minuscule
@@ -10,7 +12,7 @@ class Password {
     bool hasDigit = password.contains(RegExp(r'[0-9]'));
 
     // Retourne true si toutes les conditions sont remplies
-    return hasUppercase && hasLowercase && hasDigit;
+    return isCorrectLength && hasUppercase && hasLowercase && hasDigit;
   }
 
   String toString() {
