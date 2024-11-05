369595521a015fe62785a544780e0533fc0c892d
MarcP70 <marcpourias@yahoo.fr>
2024-11-05 17:10:20 +0100

    Add 2-main.dart and 2-password.dart for task 2 to check if the password is valide or not

---
+++ 0x02-dart-classes/2-password.dart
@@ -0,0 +1,19 @@
+class Password {
+  String password = "";
+
+  bool isValid() {
+    // Vérifie si le mot de passe contient au moins une majuscule
+    bool hasUppercase = password.contains(RegExp(r'[A-Z]'));
+    // Vérifie si le mot de passe contient au moins une minuscule
+    bool hasLowercase = password.contains(RegExp(r'[a-z]'));
+    // Vérifie si le mot de passe contient au moins un chiffre
+    bool hasDigit = password.contains(RegExp(r'[0-9]'));
+
+    // Retourne true si toutes les conditions sont remplies
+    return hasUppercase && hasLowercase && hasDigit;
+  }
+
+  String toString() {
+    return 'Your Password is: $password';
+  }
+}
